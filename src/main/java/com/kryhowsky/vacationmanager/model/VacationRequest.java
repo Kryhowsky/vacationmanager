266bc787b1a9a4afea67fe195ef629257a6c4e16
P Kry <piter71-1997@go2.pl>
2021-11-03 20:24:14 +0100

    Models

--- src/main/java/com/kryhowsky/vacationmanager/model/VacationRequest.java
+++ src/main/java/com/kryhowsky/vacationmanager/model/VacationRequest.java
@@ -1,4 +1,21 @@
 package com.kryhowsky.vacationmanager.model;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
 public class VacationRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ManyToOne
+    private VacationType vacationType;
+
+    private LocalDate startDate;
+    private LocalDate endDate;
 }
--- src/main/java/com/kryhowsky/vacationmanager/model/VacationType.java
+++ src/main/java/com/kryhowsky/vacationmanager/model/VacationType.java
@@ -4,23 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
-import java.time.LocalDate;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class VacationType {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String email;
-    private String password;
-    private LocalDate employmentDate;
-
-    @ManyToOne
-    private Role role;
+    private String name;
 }
